3fa0c03293634e36b9cd759bdfdfb511f4a6e2db
Kimleang <kimleang.hrd@gmail.com>
2021-05-03 09:56:11 +0700

    Change database to H2

---
+++ src/main/resources/scripts/scheme.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS users_roles;
+
+CREATE TABLE users
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE roles
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE users_roles
+(
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
